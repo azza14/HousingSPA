8eb2a2d110641eaebd24ad010f8d5d16b5e18009
azza <asd@gmail.com>
2020-09-13 19:26:43 +0200

    registeration

--- src/app/user/user-register/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register/user-register.component.ts
@@ -13,9 +13,11 @@ export class UserRegisterComponent implements OnInit {
   ngOnInit(): void {
     this.registerationForm= new FormGroup ({
     userName:new FormControl( 'Azza',Validators.required),
-    userPassword: new FormControl('123',Validators.required),
-    email:new FormControl(null,[Validators.required,Validators.email])
-    })
+    userPassword: new FormControl('123',[Validators.required,Validators.maxLength(8)]),
+    email:new FormControl(null,[Validators.required,Validators.email]),
+    confirmPassword: new FormControl(null, [Validators.required]),
+    mobile: new FormControl(null,[Validators.required,Validators.maxLength(11)])
+  });
   }
   onSubmit(){
 console.log(this.registerationForm)
